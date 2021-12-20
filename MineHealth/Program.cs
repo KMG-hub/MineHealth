19102ab546afa3b90bfb750caedc21adc37b70a4
KMG-hub <bming00@gmail.com>
2021-12-20 13:19:36 +0900

    에코서버 테스트용

--- MineHealth/Program.cs
+++ MineHealth/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MineHealth
 {
@@ -6,8 +9,48 @@ namespace MineHealth
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!! awesomepia! ");
-            Console.Read();
+            try
+            {
+                string strMsg;
+
+                TcpListener sockServer = new TcpListener(IPAddress.Parse("127.0.0.1"), 9090); //IP, Port
+                sockServer.Start();
+                Console.WriteLine("Server 시작! Client 연결 대기중...");
+
+                TcpClient client = sockServer.AcceptTcpClient();//Accept
+                Console.WriteLine("Client 접속성공!");
+
+                NetworkStream ns = client.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                string welcome = "Server Connnect Success!";
+                sw.WriteLine(welcome);
+                sw.Flush();
+                while (true)
+                {
+                    strMsg = sr.ReadLine();
+                    if (strMsg == "exit")  //exit 메시지 수신시 종료하기
+                        break;
+
+                    Console.WriteLine(strMsg);
+                    sw.WriteLine(strMsg);
+                    sw.Flush();
+                }
+
+                sw.Close();
+                sr.Close();
+                ns.Close();
+                sockServer.Stop();
+
+                Console.WriteLine("Client 연결 종료!");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            //Console.Read();
         }
     }
 }
