79e8bbe0d21862bb1dd1ac531858818fc8f9b4c6
KMG-hub <bming00@gmail.com>
2021-12-20 12:02:52 +0900

    초기 커밋.

---
+++ MineHealth/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MineHealth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
