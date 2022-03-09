fd97adc98c7cbdf17633d7b71b64387640c04f17
KMG-hub <bming00@gmail.com>
2022-03-09 23:53:43 +0900

    22/03/08 update

---
+++ MineHealthHttpServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Griffin.WebServer;
+
+namespace MineHealthHttpServer
+{
+    class Program
+    {
+        class User
+        {
+            public int id;
+            public string userName;
+            public User(int id, string userName)
+            {
+                this.id = id;
+                this.userName = userName;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
